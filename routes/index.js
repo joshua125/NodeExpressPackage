66286960dc708ad6c3f277a90d6a2c820c0b1aa1
Joshua Lynch <jlynch125@gmail.com>
2019-07-29 07:45:35 -0400

    Changed copy

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.post("/register", function(req, res) {
             return res.render("register");
         }
         passport.authenticate("local")(req, res ,function(){
-            req.flash("success", "Welcome To ConcreteCampers " + user.username);
+            req.flash("success", "Welcome To CampChamp " + user.username);
             res.redirect("/campgrounds");
         });
     });
